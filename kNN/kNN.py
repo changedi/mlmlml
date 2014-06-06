d4ef330cd015f3e865f3bc42d26dce1dd109193e
changedi <jysoftware@gmail.com>
2014-06-06 14:07:06 +0800

    add test for knn
    Signed-off-by: changedi <jysoftware@gmail.com>

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -53,3 +53,49 @@ def file2matrix(filename):
             classLabelVector.append(dictionary.get(listFromLine[-1]))
         index += 1
     return returnMat,classLabelVector
+
+
+## data normalize
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = normDataSet / tile(ranges, (m,1))
+    return normDataSet, ranges, minVals
+
+
+## test for dating data
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:], normMat[numTestVecs:m,:],
+                                     datingLabels[numTestVecs:m],3)
+        print 'the classifier came back with: %d, the real answer is: %d'\
+              % (classifierResult, datingLabels[i])
+        if (classifierResult != datingLabels[i]):
+            errorCount += 1.0
+    print 'the total error rate is: %f' % (errorCount/float(numTestVecs))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
