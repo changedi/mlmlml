17d88e7d2696be4d1bc7405a25842c5f7aa0429a
changedi <jysoftware@gmail.com>
2014-06-23 15:48:16 +0800

    add functions
    Signed-off-by: changedi <jysoftware@gmail.com>

--- SVM/svmMLiA.py
+++ SVM/svmMLiA.py
@@ -1,6 +1,6 @@
 from numpy import *
 
-## helper functions for the SMO
+## helper functions for the SMO.
 def loadDataSet(fileName):
     dataMat = []; labelMat = []
     fr = open(fileName)
@@ -23,7 +23,7 @@ def clipAlpha(aj,H,L):
     return aj
 
 
-## simplified SMO algorithm
+## simplified SMO algorithm.
 def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
     dataMatrix = mat(dataMatIn); labelMat = mat(classLabels).transpose()
     b = 0; m,n = shape(dataMatrix)
@@ -89,3 +89,119 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
         else: iter = 0
         print "iteration number : %d" % iter
     return b,alphas
+
+## support functions for full Platt SMO.
+class optStruct:
+    def __init__(self,dataMatIn, classLabels, C, toler):
+        self.X = dataMatIn
+        self.labelMat = classLabels
+        self.C = C
+        self.tol = toler
+        self.m = shape(dataMatIn)[0]
+        self.alphas = mat(zeros((self.m,1)))
+        self.b = 0
+        # ecahce(error cache) is a matrix of first column is a flag bit stating
+        # whether the eCache is valid, second column is E value
+        self.eCache = mat(zeros((self.m,2)))
+
+# helper function to calc E value for a given alpha.
+def calcEk(oS, k):
+    fXk = float(multiply(oS.alphas,oS.labelMat).T * \
+                (oS.X*oS.X[k,:].T)) + oS.b
+    Ek = fXk - float(oS.labelMat[k])
+    return Ek
+
+# selects the second alpha, or the inner loop alpha.
+def selectJ(i, oS, Ei):
+    maxK = -1; maxDeltaE = 0; Ej = 0
+    # first set Ei to valid in the cache
+    oS.eCache[i] = [1,Ei]
+    validEcacheList = nonzero(oS.eCache[:,0].A)[0]
+    if (len(validEcacheList)) > 1:
+        for k in validEcacheList:
+            if k == i: continue
+            Ek = calcEk(oS, k)
+            deltaE = abs(Ei - Ek)
+            if (deltaE > maxDeltaE):
+                maxK = k; maxDeltaE = deltaE; Ej = Ek
+        return maxK, Ej
+    else: # first time randomly select an alpha
+        j = selectJrand(i, oS.m)
+        Ej = calcEk(oS, j)
+    return j, Ej
+
+## calculate error and put into cache.
+def updateEk(oS, k):
+    Ek = calcEk(oS, k)
+    oS.eCache[k] = [1,Ek]
+
+## full Platt SMO optimization
+def innerL(i, oS):
+    Ei = calcEk(oS, i)
+    if ((oS.labelMat[i]*Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or \
+        ((oS.labelMat[i]*Ei > oS.tol) and (oS.alphas[i] > 0)):
+        j,Ej = selectJ(i, oS, Ei)
+        alphaIold = oS.alphas[i].copy(); alphaJold = oS.alphas[j].copy()
+        if (oS.labelMat[i] != oS.labelMat[j]):
+            L = max(0, oS.alphas[j] - oS.alphas[i])
+            H = min(oS.C, oS.C + oS.alphas[j] - oS.alphas[i])
+        else:
+            L = max(0, oS.alphas[j] + oS.alphas[i] - oS.C)
+            H = min(oS.C, oS.alphas[j] + oS.alphas[i])
+        if L==H: print "L==H"; return 0
+        eta = 2.0 * oS.X[i,:]*oS.X[j,:].T - oS.X[i,:]*oS.X[i,:].T - \
+              oS.X[j,:]*oS.X[j,:].T
+        if eta >= 0: print "eta>=0"; return 0
+        oS.alphas[j] -= oS.labelMat[j]*(Ei - Ej)/eta
+        oS.alphas[j] = clipAlpha(oS.alphas[j],H,L)
+        updateEk(oS, j)
+        if (abs(oS.alphas[j] - alphaJold) < 0.00001):
+            print "j not moving enough"; return 0
+        oS.alphas[i] += oS.labelMat[j]*oS.labelMat[i]*\
+                        (alphaJold - oS.alphas[j])
+        updateEk(oS, i)
+        b1 = oS.b - Ei - oS.labelMat[i]*(oS.alphas[i]-alphaIold)*\
+             oS.X[i,:]*oS.X[i,:].T - oS.labelMat[j]*\
+             (oS.alphas[j]-alphaJold)*oS.X[i,:]*oS.X[j,:].T
+        b2 = oS.b - Ej - oS.labelMat[i]*(oS.alphas[i]-alphaIold)*\
+             oS.X[i,:]*oS.X[j,:].T - oS.labelMat[j]*\
+             (oS.alphas[j]-alphaJold)*oS.X[j,:]*oS.X[j,:].T
+        if (0 < oS.alphas[i]) and (oS.C > oS.alphas[i]): oS.b = b1
+        elif (0 < oS.alphas[j]) and (oS.C > oS.alphas[j]): oS.b = b2
+        else: oS.b = (b1 + b2)/2.0
+        return 1
+    else: return 0
+
+## full Platt SMO outer loop
+def smoP(dataMatIn, classLabels, C, toler, maxIter, kTup=('lin', 0)):
+    oS = optStruct(mat(dataMatIn),mat(classLabels).transpose(),C,toler)
+    iter = 0
+    entireSet = True; alphaPairsChanged = 0
+    while (iter < maxIter) and ((alphaPairsChanged > 0) or (entireSet)):
+        alphaPairsChanged = 0
+        if entireSet:
+            for i in range(oS.m):
+                alphaPairsChanged += innerL(i,oS)
+            print "fullSet, iter: %d i:%d, pairs changed %d" % \
+                  (iter,i,alphaPairsChanged)
+            iter += 1
+        else:
+            nonBoundIs = nonzero((oS.alphas.A>0) * (oS.alphas.A<C))[0]
+            for i in nonBoundIs:
+                alphaPairsChanged += innerL(i,oS)
+                print "non-bound, iter: %d i:%d, pairs changed %d" % \
+                      (iter,i,alphaPairsChanged)
+            iter += 1
+        if entireSet: entireSet = False
+        elif (alphaPairsChanged == 0): entireSet = True
+        print "iteration number: %d" % iter
+    return oS.b,oS.alphas
+
+## calculating w for hyperplane
+def calcWs(alphas,dataArr,classLabels):
+    X = mat(dataArr); labelMat = mat(classLabels).transpose()
+    m,n = shape(X)
+    w = zeros((n,1))
+    for i in range(m):
+        w += multiply(alphas[i]*labelMat[i],X[i,:].T)
+    return w
