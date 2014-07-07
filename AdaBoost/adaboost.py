7da4ff8d3360081404f78ed17b0a55667919ee58
changedi <jysoftware@gmail.com>
2014-07-07 14:38:13 +0800

    mod adaboost
    Signed-off-by: changedi <jysoftware@gmail.com>

--- AdaBoost/adaboost.py
+++ AdaBoost/adaboost.py
@@ -33,8 +33,8 @@ def buildStump(dataArr, classLabels, D):
                 errArr = mat(ones((m,1)))
                 errArr[predictedVals == labelMat] = 0
                 weightedError = D.T*errArr
-                print "split: dim %d, thresh %.2f, thresh inequal: %s,\
-the weighted error is %.3f" % (i,threshVal,inequal,weightedError)
+                #print "split: dim %d, thresh %.2f, thresh inequal: %s,\
+# the weighted error is %.3f" % (i,threshVal,inequal,weightedError)
                 if weightedError < minError:
                     minError = weightedError
                     bestClasEst = predictedVals.copy()
@@ -42,3 +42,83 @@ the weighted error is %.3f" % (i,threshVal,inequal,weightedError)
                     bestStump['thresh'] = threshVal
                     bestStump['ineq'] = inequal
     return bestStump,minError,bestClasEst
+
+# AdaBoost training with decision stump
+def adaBoostTrainDS(dataArr, classLabels, numIt=40):
+    weakClassArr = []
+    m = shape(dataArr)[0]
+    D = mat(ones((m,1))/m)  # weight of each piece of data and initialized with 1/m
+    aggClassEst = mat(zeros((m,1)))
+    for i in range(numIt):
+        bestStump, error, classEst = buildStump(dataArr, classLabels, D)
+        print "D:",D.T
+        # alpha tell the classifier how much to weight the output from this stump
+        alpha = float(0.5*log((1.0-error)/max(error,1e-16)))
+        bestStump['alpha'] = alpha
+        weakClassArr.append(bestStump)
+        print "classEst: ",classEst.T
+        # calculate new D
+        expon = multiply(-1*alpha*mat(classLabels).T, classEst)
+        D = multiply(D, exp(expon))
+        D = D/D.sum()
+        aggClassEst += alpha*classEst
+        print "aggClassEst: ", aggClassEst.T
+        aggErrors = multiply(sign(aggClassEst)!=mat(classLabels).T,ones((m,1)))
+        errorRate = aggErrors.sum()/m
+        print "total error: ",errorRate,"\n"
+        if errorRate == 0.0: break
+    return weakClassArr,aggClassEst
+
+# AdaBoost classification
+def adaClassify(datToClass, classifierArr):
+    dataMatrix = mat(datToClass)
+    m = shape(dataMatrix)[0]
+    aggClassEst = mat(zeros((m,1)))
+    for i in range(len(classifierArr)):
+        classEst = stumpClassify(dataMatrix, classifierArr[i]['dim'],\
+                                 classifierArr[i]['thresh'],\
+                                 classifierArr[i]['ineq'])
+        aggClassEst += classifierArr[i]['alpha']*classEst
+        print aggClassEst
+    return sign(aggClassEst)
+
+# Adaptive load data
+def loadDataSet(fileName):
+    numFeat = len(open(fileName).readline().split('\t'))
+    dataMat = []; labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr = []
+        curLine = line.strip().split('\t')
+        for i in range(numFeat-1):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat, labelMat
+
+# ROC plotting and AUC calculating
+def plotROC(predStrengths, classLabels):
+    import matplotlib.pyplot as plt
+    cur = (1.0,1.0)
+    ySum = 0.0
+    numPosClas = sum(array(classLabels)==1.0)
+    yStep = 1/float(numPosClas)
+    xStep = 1/float(len(classLabels)-numPosClas)
+    sortedIndicies = predStrengths.argsort()
+    fig = plt.figure()
+    fig.clf()
+    ax = plt.subplot(111)
+    for index in sortedIndicies.tolist()[0]:
+        if classLabels[index] == 1.0:
+            delX = 0; delY = yStep;
+        else:
+            delX = xStep; delY = 0;
+            ySum += cur[1]
+        ax.plot([cur[0],cur[0]-delX],[cur[1],cur[1]-delY], c='b')
+        cur = (cur[0]-delX,cur[1]-delY)
+    ax.plot([0,1],[0,1],'b--')
+    plt.xlabel('False Positive Rate'); plt.ylabel('True Positive Rate')
+    plt.title('ROC curve for AdaBoost Horse Colic Detection System')
+    ax.axis([0,1,0,1])
+    plt.show()
+    print "the Area Under the Curve is: ", ySum*xStep
