c5bf9ebfc5f705843df3fe81de800612792edb0b
Jia Yu <jysoftware@gmail.com>
2023-08-21 21:42:42 +0800

    Add files via upload

---
+++ generator.py
@@ -0,0 +1,15 @@
+def calc_avg():
+    """流式计算平均值"""
+    total, counter = 0, 0
+    avg_value = None
+    while True:
+        value = yield avg_value
+        total, counter = total + value, counter + 1
+        avg_value = total / counter
+
+
+gen = calc_avg()
+next(gen)
+print(gen.send(10))
+print(gen.send(20))
+print(gen.send(30))
